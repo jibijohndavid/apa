b212f545cb4232e4c19ed4b9e9e1f63bd98fe7d0
jibijohndavid <jibijohndavid@gmail.com>
2019-10-06 14:23:35 +0530

    travertine type fixed

--- src/components/products-list.js
+++ src/components/products-list.js
@@ -12,49 +12,49 @@ const ProductList = () => (
       <div className="products">
         <ProductListItem
           category="Marbles"
-          img={path + "marbles-bruno-white.jpg"}
+          img={path + "Marbles/bruno-white.jpg"}
           title="Bruno White"
         ></ProductListItem>
         <ProductListItem
           category="Marbles"
-          img={path + "marbles-ocean-black.jpg"}
+          img={path + "Marbles/ocean-black.jpg"}
           title="Ocean Black"
         ></ProductListItem>
         <ProductListItem
           category="Granites"
-          img={path + "granite-ivory-fantasy.jpg"}
+          img={path + "Granites/ivory-fantasy.jpg"}
           title="Ivory fantasy"
         ></ProductListItem>
         <ProductListItem
           category="Granites"
-          img={path + "granite-lavendar-blue.jpg"}
+          img={path + "Granites/lavendar-blue.jpg"}
           title="Lavender Blue"
         ></ProductListItem>
         <ProductListItem
           category="Granites"
-          img={path + "granite-kashmire-white.jpg"}
+          img={path + "Granites/kashmir-white.jpg"}
           title="Kashmire White"
         ></ProductListItem>
         <ProductListItem
           category="Travertine"
-          img={path + "travertine-beige.jpg"}
+          img={path + "Travertine/beige.jpg"}
           title="Beige"
         ></ProductListItem>
-        {/* <ProductListItem
+        <ProductListItem
           category="Travertine"
-          img={path + "travertine-silver.jpg"}
+          img={path + "Travertine/silver.jpg"}
           title="Silver"
         ></ProductListItem>
         <ProductListItem
           category="Blinders"
-          img={path + "blinders-zebra-roller.jpg"}
+          img={path + "Blinders/zebra-roller.jpg"}
           title="Zebra Roller Blinds"
         ></ProductListItem>
         <ProductListItem
           category="Blinders"
-          img={path + "blinders-wooden.jpg"}
+          img={path + "Blinders/wooden.jpg"}
           title="Wooden Blinds"
-        ></ProductListItem> */}
+        ></ProductListItem>
       </div>
       <Link to="/products" className="cta__action">
         View All Products
