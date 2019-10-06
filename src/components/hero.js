1b9fcc0a8b77a702a3524b20b2f8d745ac702060
jibijohndavid <jibijohndavid@gmail.com>
2019-10-06 13:40:10 +0530

    hero section updated

--- src/components/hero.js
+++ src/components/hero.js
@@ -4,7 +4,7 @@ import Slider from "react-slick"
 var settings = {
   dots: false,
   arrow: false,
-  autoplay: true,
+  // autoplay: true,
   autoplaySpeed: 3500,
   pauseOnFocus: false,
   infinite: true,
@@ -17,36 +17,56 @@ const Hero = () => (
       <div>
         <section className="hero-wrapper slide1">
           <div className="container">
-            <h1 className="hero__title">
-              A design culture that transforms experience
-            </h1>
+            <div className="hero__title">
+              <h1>A design culture that transforms experience</h1>
+              <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Possimus quam fugit quae quia tempora officia, blanditiis,
+                sapiente veritatis dolorem autem eaque dolores animi.
+              </p>
+            </div>
           </div>
         </section>
       </div>
       <div>
         <section className="hero-wrapper slide2">
           <div className="container">
-            <h1 className="hero__title">
-              A design culture that transforms experience
-            </h1>
+            <div className="hero__title">
+              <h1>A design culture that transforms experience</h1>
+              <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Possimus quam fugit quae quia tempora officia, blanditiis,
+                sapiente veritatis dolorem autem eaque dolores animi.
+              </p>
+            </div>
           </div>
         </section>
       </div>
       <div>
         <section className="hero-wrapper slide3">
           <div className="container">
-            <h1 className="hero__title">
-              A design culture that transforms experience
-            </h1>
+            <div className="hero__title">
+              <h1>A design culture that transforms experience</h1>
+              <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Possimus quam fugit quae quia tempora officia, blanditiis,
+                sapiente veritatis dolorem autem eaque dolores animi.
+              </p>
+            </div>
           </div>
         </section>
       </div>
       <div>
         <section className="hero-wrapper slide4">
           <div className="container">
-            <h1 className="hero__title">
-              A design culture that transforms experience
-            </h1>
+            <div className="hero__title">
+              <h1>A design culture that transforms experience</h1>
+              <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Possimus quam fugit quae quia tempora officia, blanditiis,
+                sapiente veritatis dolorem autem eaque dolores animi.
+              </p>
+            </div>
           </div>
         </section>
       </div>
